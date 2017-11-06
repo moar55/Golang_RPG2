ff849d1b3e5c8a1209e9a008471b0262d9c4f842
Omar Hossam <moar.ahmed@gmail.com>
2017-11-06 21:54:43 +0200

    .

--- main.go
+++ main.go
@@ -5,8 +5,23 @@ import (
 	_ "Golang_RPG/scripts"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
 func main() {
+	// https://github.com/astaxie/beego/issues/1037
+	beego.InsertFilter("*", beego.BeforeRouter, func(ctx *context.Context) {
+		if ctx.Input.Method() == "OPTIONS" {
+			ctx.Output.Header("Access-Control-Allow-Origin", "*")
+			ctx.Output.Header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT")
+			ctx.Output.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+			ctx.Abort(200, "Hello")
+		}
+	})
+
+	if beego.RunMode == "dev" {
+		beego.DirectoryIndex = true
+		beego.StaticDir["/swagger"] = "swagger"
+	}
 	beego.Run()
 }
